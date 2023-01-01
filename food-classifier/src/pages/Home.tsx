16946a6ee84ceda4d45a381217f9c2b18896baf7
MASHAR27 <iamashar995@gmail.com>
2023-01-01 06:00:02 +0000

    code finished for color, wait for trails

--- food-classifier/src/pages/Home.tsx
+++ food-classifier/src/pages/Home.tsx
@@ -5,6 +5,13 @@ import './Home.css';
 import ImageUpload from "../components/Imageupload"
 import React from 'react';
 
+
+const colorList=[
+  "#453C67",
+  "#6D67E4",
+  "#46C2CB",
+  "#F2F7A1"
+];
 const Home: React.FC = () => {
   return (
     <>
@@ -31,25 +38,30 @@ const Home: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <IonContent>
-          <IonCard>
-            <IonCardHeader>
-              <img src="https://img.freepik.com/free-photo/top-view-pepperoni-pizza-with-mushroom-sausages-bell-pepper-olive-corn-black-wooden_141793-2158.jpg?w=2000"></img>
-              <IonCardTitle>Name</IonCardTitle>
 
-            </IonCardHeader>
+          {[1, 2, 3, 4, 5, 6, 7].map((key) =>
+            <IonCard style={{backgroundColor: colorList[key%colorList.length]}}>
+              <IonCardHeader>
+                <IonCardTitle>Name</IonCardTitle>
+              </IonCardHeader>
+              <IonCardContent className="text-lg">
+                <b className="font-bold mb-1 inline-block">Type: </b> Fruit
+                <br />
+                <b className="font-bold mb-1 inline-block">Date: </b> Dec 22, 2022
+                <br />
+                <b className="font-bold mb-1 inline-block">Time: </b> 12:10 AM
+              </IonCardContent>
+            </IonCard>
+          )}
 
-            <IonCardContent>
-              Class
-            </IonCardContent>
 
-          </IonCard>
 
         </IonContent>
 
         <input accept='image/*' hidden id="uploader" type="file" onChange={(e: React.ChangeEvent<HTMLInputElement>) => { //type matters
           console.log(e.target.files![0])
           const url = URL.createObjectURL(e.target.files![0]);
-          
+          // HappieEE123/Project_310
         }} />
 
         <IonFab slot="fixed" vertical="bottom" horizontal="end" style={{ marginRight: "15px", marginBottom: "10px" }}>
